a83adf823a95c857fe0428e05153cc89f0e669d7
Ilnar Agzamov <agzamov001@gmail.com>
2023-03-23 09:22:25 +0500

    id_ingredient в storage
    Опять осталось не то написание атрибута id_ingredient в storage. Пришлось опять менять. Убрал остатки закомментированных строчек в oruz.py

--- fastapi/application/models/dao/oruz.py
+++ fastapi/application/models/dao/oruz.py
@@ -13,48 +13,34 @@ Base = declarative_base()
 class Orders(Base):
     __tablename__ = "orders"
     id = Column(Integer, nullable=False, primary_key=True)
-    date = Column(DateTime(), default=datetime.now, nullable=False) # в поле автоматически генерируется метка времени при создании записи, метка обновляется при каждой операции UPDATE
-    # ....
-# class Ingredients(Base):
-#     __tablename__ = "ingredients"
-#
-#     id = Column(Integer, nullable=True, primary_key=True)
-#     name = Column(String(255), nullable=False, unique=True)
-#     count = Column(Integer, nullable=False, unique=False)
+    date = Column(DateTime(), default=datetime.now, nullable=False) 
+
 class Ingredients(Base):
     __tablename__ = 'ingredients'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), nullable=False)
     count = Column(Integer, default=0)
-    # ....
+
 class Dishes(Base):
     __tablename__ = "dishes"
-
     id = Column(Integer, nullable=True, primary_key=True)
     name = Column(String(255), nullable=False, unique=True)
-    #order = ....
-    #ingredient = ....
+
 class Storage(Base):
     __tablename__ = "storage"
     id = Column(Integer, nullable=True, primary_key=True)
     id_ingredient = Column(Integer, ForeignKey('ingredients.id'), nullable=False)
     count = Column(Integer, nullable=False, unique=False)
     expiry_date = Column(Date())
-    #...
+
 class DishesIngredients(Base):
     __tablename__ = 'dishes_ingredients'
-
-   # id = Column(Integer, primary_key=True)
-
     id_dish = Column(Integer, ForeignKey('dishes.id'), nullable=False, primary_key=True)
     id_ingredient = Column(Integer, ForeignKey('ingredients.id'),primary_key=True)
     amount = Column(Integer, nullable=False, unique=False)
 
 class OrdersDishes(Base):
     __tablename__ = 'orders_dishes'
-
-   # id = Column(Integer, primary_key=True)
-
     id_dish = Column(Integer, ForeignKey('dishes.id'), nullable=False, primary_key=True)
     id_order = Column(Integer, ForeignKey('ingredients.id'),primary_key=True)
     amount = Column(Integer, nullable=False, unique=False)
